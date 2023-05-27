797437c1387d0ab5bdb4402fad739d28f3f1caaf
EduardoP103 <eduardopastor103@gmail.com>
2023-05-27 00:21:05 -0500

    Backend Cliente Listar

---
+++ src/main/java/com/tiendaOnline/springboot/app/DeliveryRapidWorkApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaOnline.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryRapidWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryRapidWorkApplication.class, args);
+	}
+
+}
